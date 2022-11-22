3d9cb499ce4e7bbf1fcfa867674d6b3d90b244c3
amandawei00 <amandawei00@gmail.com>
2022-11-22 13:40:55 -0800

    new plots

--- ad.py
+++ ad.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 from matplotlib.animation import PillowWriter
 
+plt.rcParams.update({'font.size': 18.})
 args = {'R1':1.3,'R2':3.44,'R3':0.043,'R4':0.193,'alphaf':11.6,'alphar':11.6,'c':4.4,'I':22.5*10**(-6)}#R1,R2,R3,R4,alphaf,alphar,c,Ir
 
 def Derivative(Params,t,args=args):
@@ -20,8 +21,9 @@ def G(V,args=args):
 
 
 t = 23.5*10**(-6)*np.linspace(0,1e8,int(1e5)) #Setting the time
-# Params = np.array([1.2,1. ,0.3*10**(-3),1.,1.,0.3*10**(-3)]) #setting the parameters, Note: It may be important to play with the initial conditions a bit
-Params = np.array([1,1, 0.3 * 10**(-3),1, 1, 0.3 * 10**(-3)])
+# Params = np.array([1.2,1. ,0.3*10**(-3),1.,1.,0.3*10**(-3)])
+# Params = np.array([0.0001, 0.0001,0.3*10**(-3), 0.000001, 0.000001, 0.8*10**(-3)]) # plot 3
+Params = np.array([1,1, 0.3 * 10**(-3),1, 1, 0.3 * 10**(-3)]) # plot 2
 states = odeint(Derivative,Params,t)   #Solving the odeint
 Xpar = (states[:,:3]+states[:,3:6])/2  #Perpendicular Projections for our 3d space
 Xperp = (states[:,:3]-states[:,3:6])/2 #Parallel Projections for our 3d space
@@ -33,6 +35,9 @@ num = int(1e4)
 ax.set(xlabel='XPERP1', ylabel='XPAR1', zlabel='XPAR3')
 ax.scatter3D(Xperp[:,0][:num], Xpar[:,0][:num], Xpar[:,2][:num])
 
+ax.set_xticks([-1.5e-13, -0.75e-13, 0.])
+ax.set_yticks([-2., -1., 0., 1., 2.])
+# ax.set_zticks()
 plt.show()
 
 '''
